4474c1547be34fe2cb344acc0c38b7f605d79867
davudxD <carovacdavud6@gmail.com>
2023-05-22 20:43:56 +0200

    teams app

---
+++ teams-app/src/App.js
@@ -0,0 +1,79 @@
+import React from "react";
+import "./App.css";
+import ScoreCard from "./components/ScoreCard";
+
+class App extends React.Component {
+  state = {
+    scores: [],
+    gameover: false,
+    player: "",
+  };
+
+  goalScored(team, event) {
+    const eventData = {
+      player: this.state.player,
+      team: team,
+      event: event,
+    };
+    console.log(eventData)
+    const newScores = this.state.scores;
+    newScores.push(eventData);
+    this.setState({ scores: newScores, player: "" });
+  }
+
+  getGoalNumber(team) {
+    const teamScores = this.state.scores.filter((item, index) => {
+      return item.team === team && item.event === "goal";
+    });
+    console.log(teamScores)
+    return teamScores.length;
+  }
+
+  render() {
+    const homeScores = this.getGoalNumber("home");
+    const guestScores = this.getGoalNumber("guest");
+    return (
+      <div className="app">
+        <div className="scores-container">
+          <ScoreCard
+            name="Home"
+            score={homeScores}
+            goal={() => {
+              this.goalScored("home", "goal");
+            }}
+            yellowCard={() => {
+              this.goalScored("home", "yellow card")
+            }}
+              redCard={() => {
+              this.goalScored("home", "red card")
+            }}
+          />
+          <ScoreCard
+            name="Guest"
+            score={guestScores}
+            goal={() => {
+              this.goalScored("guest", "goal");
+            }}
+            yellowCard={() => {
+              this.goalScored("guest", "yellow card")
+            }}
+            redCard={() => {
+              this.goalScored("guest", "red card")
+            }}
+          />
+        </div>
+        <input
+          value={this.state.player}
+          onChange={(event) => {
+            const value = event.target.value;
+            this.setState({ player: value });
+          }}
+          placeholder="Unesite ime fudbalera"
+          
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
