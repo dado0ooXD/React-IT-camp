10836088b6c617167e55e876ed041117cee449a1
davudxD <carovacdavud6@gmail.com>
2023-05-27 10:56:01 +0200

    instagram clone app

--- instagram-clone-app/src/components/Post.js
+++ instagram-clone-app/src/components/Post.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
 
-const Post = () => {
+const Post = (props) => {
   return (
-    <div>
-      
+    <div className="post">
+      <div>
+        <img className="post-img" src={props.post.imageUrl}></img>
+      </div>
+      <div className="post-buttons">
+        <button
+          onClick={() => {
+            props.likeHandler(props.post.id);
+          }}
+        >
+          Like
+        </button>
+        <button
+          onClick={() => {
+            console.log("comment");
+          }}
+        >
+          Comment
+        </button>
+        <button
+          onClick={() => {
+            console.log("share");
+          }}
+        >
+          Share
+        </button>
+      </div>
+      <div>
+        <p className="post-likes">{props.post.likes.length}</p>
+        <p className="post-description">
+          <span className="username">{props.post.username}</span>{" "}
+          <span>{props.post.description}</span>
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
