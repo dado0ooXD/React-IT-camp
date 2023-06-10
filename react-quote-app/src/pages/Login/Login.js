fc9c69ca9cc360397a2c2bd94b4067630a2ad920
davudxD <carovacdavud6@gmail.com>
2023-06-10 12:37:56 +0200

    loading al nece

--- react-quote-app/src/pages/Login/Login.js
+++ react-quote-app/src/pages/Login/Login.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Login.css";
 import { Formik } from "formik";
 import * as yup from "yup";
 import { useNavigate } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
 
 const loginSchema = yup.object({
   email: yup.string().required("Nedostaje email").email("Neispravan email"),
@@ -12,12 +13,14 @@ const loginSchema = yup.object({
 
 const Login = () => {
   const navigate = useNavigate();
+  
 
   return (
     <div className="login-wrapper">
       <Formik
-        initialValues={{ email: "", password: "" }}
+        initialValues={{ email: "", password: "", isLoading: false }}
         onSubmit={(values, actions) => {
+       
           fetch("https://js-course-server.onrender.com/user/login", {
             method: "POST",
             body: JSON.stringify(values),
@@ -27,8 +30,10 @@ const Login = () => {
           })
             .then((res) => res.json())
             .then((data) => {
+              values.isLoading = true
               if (data.token) {
                 localStorage.setItem("auth_token", data.token);
+                
                 alert(data.success)
                 navigate("/addnewquote");
               }             
@@ -55,44 +60,46 @@ const Login = () => {
           handleBlur,
           handleSubmit,
         }) => (
+          !values.isLoading ? (<div>
+          <button
+            onClick={() => {
+              console.log(values, "values");
+              console.log(errors, "errors");
+              console.log(touched, "touched");
+            }}
+          >
+            Console log states
+          </button>
           <div>
-            <button
-              onClick={() => {
-                console.log(values, "values");
-                console.log(errors, "errors");
-                console.log(touched, "touched");
-              }}
-            >
-              Console log states
-            </button>
-            <div>
-              <input
-                type="email"
-                name="email"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.email}
-              />
-              <p className="error-message">
-                {errors.email && touched.email && errors.email}
-              </p>
-            </div>
-            <div>
-              <input
-                type="password"
-                name="password"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.password}
-              />
-              <p className="error-message">
-                {errors.password && touched.password && errors.password}
-              </p>
-            </div>
-            <button onClick={handleSubmit} type="button">
-              Submit
-            </button>
+            <input
+              type="email"
+              name="email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+            />
+            <p className="error-message">
+              {errors.email && touched.email && errors.email}
+            </p>
           </div>
+          <div>
+            <input
+              type="password"
+              name="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+            />
+            <p className="error-message">
+              {errors.password && touched.password && errors.password}
+            </p>
+          </div>
+          <button onClick={handleSubmit} type="button">
+            Submit
+          </button>
+        </div>) : (<div className="spinner-border text-primary" role="status">
+  <span className="visually-hidden">Loading...</span>
+</div>)
         )}
       </Formik>
     </div>
