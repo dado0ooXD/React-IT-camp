32eefc53ac622940ce0bc66ea7eeff98cf6fc500
davudxD <carovacdavud6@gmail.com>
2023-06-10 12:46:14 +0200

    Loading spinner adding

--- react-quote-app/src/pages/Login/Login.js
+++ react-quote-app/src/pages/Login/Login.js
@@ -1,9 +1,9 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import { Formik } from "formik";
 import * as yup from "yup";
 import { useNavigate } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 const loginSchema = yup.object({
   email: yup.string().required("Nedostaje email").email("Neispravan email"),
@@ -13,14 +13,12 @@ const loginSchema = yup.object({
 
 const Login = () => {
   const navigate = useNavigate();
-  
 
   return (
     <div className="login-wrapper">
       <Formik
         initialValues={{ email: "", password: "", isLoading: false }}
         onSubmit={(values, actions) => {
-       
           fetch("https://js-course-server.onrender.com/user/login", {
             method: "POST",
             body: JSON.stringify(values),
@@ -30,13 +28,10 @@ const Login = () => {
           })
             .then((res) => res.json())
             .then((data) => {
-              values.isLoading = true
               if (data.token) {
                 localStorage.setItem("auth_token", data.token);
-                
-                alert(data.success)
                 navigate("/addnewquote");
-              }             
+              }
             });
         }}
         validationSchema={loginSchema}
@@ -59,48 +54,55 @@ const Login = () => {
           handleChange,
           handleBlur,
           handleSubmit,
-        }) => (
-          !values.isLoading ? (<div>
-          <button
-            onClick={() => {
-              console.log(values, "values");
-              console.log(errors, "errors");
-              console.log(touched, "touched");
-            }}
-          >
-            Console log states
-          </button>
-          <div>
-            <input
-              type="email"
-              name="email"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.email}
-            />
-            <p className="error-message">
-              {errors.email && touched.email && errors.email}
-            </p>
-          </div>
-          <div>
-            <input
-              type="password"
-              name="password"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.password}
-            />
-            <p className="error-message">
-              {errors.password && touched.password && errors.password}
-            </p>
-          </div>
-          <button onClick={handleSubmit} type="button">
-            Submit
-          </button>
-        </div>) : (<div className="spinner-border text-primary" role="status">
-  <span className="visually-hidden">Loading...</span>
-</div>)
-        )}
+        }) =>
+          values.isLoading ? (
+            <div className="spinner-border text-primary" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          ) : (
+            <div>
+              <button
+                onClick={() => {
+                  console.log(values, "values");
+                  console.log(errors, "errors");
+                  console.log(touched, "touched");
+                }}
+              >
+                Console log states
+              </button>
+              <div>
+                <input
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                />
+                <p className="error-message">
+                  {errors.email && touched.email && errors.email}
+                </p>
+              </div>
+              <div>
+                <input
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                />
+                <p className="error-message">
+                  {errors.password && touched.password && errors.password}
+                </p>
+              </div>
+                <button onClick={() => {
+                  values.isLoading = true;
+                  handleSubmit()
+              }} type="button">
+                Submit
+              </button>
+            </div>
+          )
+        }
       </Formik>
     </div>
   );
