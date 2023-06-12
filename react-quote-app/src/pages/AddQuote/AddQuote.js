884d5a59937e97ab9a2ec2d008ce543b683b3c1f
davudxD <carovacdavud6@gmail.com>
2023-06-12 14:47:42 +0200

    changes

--- react-quote-app/src/pages/AddQuote/AddQuote.js
+++ react-quote-app/src/pages/AddQuote/AddQuote.js
@@ -1,75 +1,79 @@
-import React, {  useState } from 'react';
-import { useNavigate, useParams } from 'react-router';
-import './AddQuote.css'
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router";
+import "./AddQuote.css";
 
 const AddQuote = () => {
-    const params = useParams();
-    const navigate = useNavigate();
-    
-    const [author, setAuthor] = useState("");
-    const [text, setText] = useState("");
-    const [source, setSource] = useState("");
-    const  [category, setCategory ] = useState("hardkodirano");
+  const params = useParams();
+  const navigate = useNavigate();
 
-        const addQuote = {
-            quoteText: text,
-            quoteSource: source,
-            quoteAuthor: author,
-            category: category,
-    };
-    
-    const addNewQuote = () => {
-        fetch("https://js-course-server.onrender.com/quotes/add-quote", {
-            method: "POST",
-            body: JSON.stringify(addQuote),
-            headers: {
-                "Content-Type": "application/json",
-                authorization: localStorage.getItem("auth_token")
-            },
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                    console.log(data)
-                alert("Citat je uspesno dodat!");
-                navigate("/");
-                
-            })
-            .catch(() => {
-                alert("error");
-            });
-    };
+  const [author, setAuthor] = useState("");
+  const [text, setText] = useState("");
+  const [source, setSource] = useState("");
+  const [category, setCategory] = useState("hardkodirano");
 
-    
-    return (
-        <div className="main">
-            <input
-                name='quoteText'
-                placeholder ="Quote Text"
-                value={text}
-                onChange={(e) => {
-                    setText(e.target.value);
-                    console.log(text);
-                }}
-            />
-            <input
-                name='quoteAuthor'
-                placeholder="Quote Author"
-                value={author}
-                onChange={(e) => {
-                    setAuthor(e.target.value);
-                }}
-            />
-            <input
-                name='quoteSource'
-                placeholder="Quote Source"
-                onChange={(e) => {
-                    setSource(e.target.value);
-                }}
-            />
+  const addQuote = {
+    quoteText: text,
+    quoteSource: source,
+    quoteAuthor: author,
+    category: category,
+  };
 
-            <button onClick={addNewQuote}>Add</button>
-        </div>
-    )
-}
+  const addNewQuote = () => {
+    fetch("https://js-course-server.onrender.com/quotes/add-quote", {
+      method: "POST",
+      body: JSON.stringify(addQuote),
+      headers: {
+        "Content-Type": "application/json",
+        authorization: localStorage.getItem("auth_token"),
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        alert("Citat je uspesno dodat!");
+        navigate("/");
+      })
+      .catch(() => {
+        alert("error");
+      });
+  };
+
+  return (
+    <div className="main">
+      <input
+        name="quoteText"
+        placeholder="Quote Text"
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value);
+          console.log(text);
+        }}
+      />
+      <input
+        name="quoteAuthor"
+        placeholder="Quote Author"
+        value={author}
+        onChange={(e) => {
+          setAuthor(e.target.value);
+        }}
+      />
+      <input
+        name="quoteSource"
+        placeholder="Quote Source"
+        onChange={(e) => {
+          setSource(e.target.value);
+        }}
+      />
+
+      <button
+        className="addquote-btn"
+        onClick={addNewQuote}
+        disabled={text === "" || author === "" || source === ""}
+      >
+        Add Quote
+      </button>
+    </div>
+  );
+};
 
 export default AddQuote;
