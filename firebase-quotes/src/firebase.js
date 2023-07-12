1a2296b15326ab55fe7b556d88013f1ec876df7d
davudxD <carovacdavud6@gmail.com>
2023-07-12 20:49:23 +0200

    firebase import

--- firebase-quotes/src/firebase.js
+++ firebase-quotes/src/firebase.js
@@ -1,7 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
-import { addDoc, doc, setDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  getDoc,
+  doc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
