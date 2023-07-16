fd117b01750b3f78a54765b7b44fe3af96a94717
davudxD <carovacdavud6@gmail.com>
2023-07-16 21:26:39 +0200

    edit page - everything works  :) :) :)

--- firebase-todo-list/src/pages/Edit.js
+++ firebase-todo-list/src/pages/Edit.js
@@ -1,30 +1,61 @@
-import React, {useState, useEffect} from 'react';
-import { Box, Typography, TextField, Button } from '@mui/material';
-import { useParams} from "react-router-dom";
-import { getItemById } from '../firebase';
+import React, { useState, useEffect } from "react";
+import { Box, Typography, TextField, Button } from "@mui/material";
+import { useParams, useNavigate } from "react-router-dom";
+import { getItemById, updateTodoItemData } from "../firebase";
 
 const Edit = () => {
-    const [updatedTask, setUpdatedTask] = useState("");
-    const params = useParams();
+  const [updatedTask, setUpdatedTask] = useState("");
+  const params = useParams();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        getItemById(params.id).then((data) => {
-            console.log(data);
-            setUpdatedTask(data.title)
-        })
-    }, [])
+  useEffect(() => {
+    getItemById(params.id).then((data) => {
+      console.log(data);
+      setUpdatedTask(data.title);
+    });
+  }, []);
+    
+    
+    const submitEdit = () => {
+        updateTodoItemData(params.id, { title: updatedTask });
+        navigate("/")
+    }
 
   return (
-      <Box height = "100vh" display= "flex" justifyContent= "center" alignItems = "center">
-          <Box style = {{backgroundColor: "#f3f3f3", padding: "30px"}} display= "flex" justifyContent= "center" alignItems = "center" flexDirection= "column">
-              <Typography variant = "h4">Edit your task</Typography>
-              <Box display = "flex" marginTop = "20px" flexDirection = "column">
-                  <TextField value = {updatedTask} onChange={e => setUpdatedTask(e.target.value)} type="text" />
-                  <Button variant = "contained" color = "success" style = {{marginTop: "10px"}}>Submit</Button>
-              </Box>
-          </Box>
-   </Box>
-  )
-}
+    <Box
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Box
+        style={{ backgroundColor: "#f3f3f3", padding: "30px" }}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+      >
+        <Typography variant="h4">Edit your task</Typography>
+        <Box display="flex" marginTop="20px" flexDirection="column">
+          <TextField
+            value={updatedTask}
+            onChange={(e) => setUpdatedTask(e.target.value)}
+            type="text"
+          />
+          <Button
+            variant="contained"
+            color="success"
+                      style={{ marginTop: "10px" }}
+                      onClick={() => {
+                          submitEdit()
+                      }}
+          >
+            Submit
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
 
-export default Edit
+export default Edit;
